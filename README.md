5ade876d8be9a9f551b54f45f7c2f41af8346042
aayushi <maheshwari.aa@husky.neu.edu>
2018-05-30 23:02:20 -0400

    Integrating Spring Boot with MySQL

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Integrating Spring Boot with MySQL
 [Insert a static hello message](http://cs5200.us-east-2.elasticbeanstalk.com/api/hello/insert)
-[Insert a parameterized hello message](http://cs5200.us-east-2.elasticbeanstalk.com/api/hello/insert/Hello! You inserted this message.)
+[Insert a parameterized hello message](http://cs5200.us-east-2.elasticbeanstalk.com/api/hello/insert/Hello all!)
 [Retrieve all hello messages](http://cs5200.us-east-2.elasticbeanstalk.com/api/hello/select/all)
--- src/main/java/edu/northeastern/cs5200/Cs5200Summer2018MaheshwariApplication.java
+++ src/main/java/edu/northeastern/cs5200/Cs5200Summer2018MaheshwariApplication.java
@@ -2,9 +2,18 @@ package edu.northeastern.cs5200;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Cs5200Summer2018MaheshwariApplication {
+public class Cs5200Summer2018MaheshwariApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder
+		configure(SpringApplicationBuilder application) {
+		return application.sources(
+			Cs5200Summer2018MaheshwariApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cs5200Summer2018MaheshwariApplication.class, args);
